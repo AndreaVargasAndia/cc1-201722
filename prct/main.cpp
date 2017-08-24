4c7d4cb7efb2dfe5f4d3be0cf7fd71ba984fdcfe
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-24 11:11:14 -0500

    trabajo

--- prct/main.cpp
+++ prct/main.cpp
@@ -4,17 +4,19 @@ using namespace std;
 
 int main()
 {
-    int n,m,t,b=0;
+    int x,a,b,c=0;
     cout << "ingresa un numero" <<endl;
-    cin>>n;
-    for(m=1;m<=n;m=m+1){
-            t=n%m;
-            if(t==0)
-                b=b+1;}
-            if (b>2){
-                cout<<"No es primo"<<endl;}
-            else{
+    cin>>x;
+    while (x!=0)
+    for(a=0;a<x+1;a=a+1){
+        b=x%a;
+        if(b==0)
+            c=c+1;}
+
+            if (c==2){
                 cout<<"Es primo"<<endl;}
+            else{
+                cout<<"No es primo"<<endl;}
     return 0;
 
 }
